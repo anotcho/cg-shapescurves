4bd54e7bfc6a72ec36c143e38c48a7bb5e069f78
anotcho <55111098+anotcho@users.noreply.github.com>
2020-02-21 01:01:36 -0600

    Update renderer.js

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -87,7 +87,8 @@ class Renderer {
 		this.drawLine({x: 350, y: 300}, {x: 450, y: 300}, [0,255,0,255],framebuffer);
 		
 		this.drawBezierCurve({x: 465, y: 250}, {x: 460, y: 310}, {x: 565, y: 310}, {x: 565, y: 280}, [0,0,255,255],framebuffer);
-		this.drawBezierCurve({x: 465, y: 250}, {x: 460, y: 190}, {x: 565, y: 190}, {x: 565, y: 210}, [0,0,255,255],framebuffer);
+		this.drawBezierCurve({x: 465, y: 250}, {x: 460, y: 190}, {x: 565, y: 190}, {x: 565, y: 220}, [0,0,255,255],framebuffer);
+		this.drawLine({x: 5650, y: 280}, {x: 565, y: 300}, [0,0,255,255],framebuffer);
 		
 		this.drawLine({x: 600, y: 200}, {x: 600, y: 400}, [255,0,255,255],framebuffer);
     }
